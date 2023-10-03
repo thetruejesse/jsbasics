0e12722f187fdddec2b9080b3c8f670f04a7ac94
Jesse Isooba <jesse4isooba@gmail.com>
2023-10-03 11:05:26 +0300

    Excercise 3 posted

---
+++ excercise3.js
@@ -0,0 +1,47 @@
+// for loop - count up to 5 and say "counting completed"
+
+for (i = 1; i <= 5; i++)
+{
+    console.log(i);
+}
+console.log("Counting completed!");
+
+// for loop - count down to 1 and say "countdown finished"
+
+for (j = 5; j >= 1; j--)
+{
+    console.log(j);
+}
+console.log("Countdown finished!");
+
+// while loop - count up to 5 and say "counting completed"
+
+var k = 1;
+
+while(k <= 5)
+{
+    console.log(k);
+    k++;
+}
+console.log("Counting completed!");
+
+// while loop - count down to 1 and say "countdown finished"
+
+var a = 5;
+
+while(a >= 1)
+{
+    console.log(a);
+    a--;
+}
+console.log("Countdown finished!");
+
+// while loop - code counting through the years
+
+var year = 2018;
+
+while (year <= 2022)
+{
+    console.log(year);
+    year++;
+}

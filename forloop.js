d9421e3f8dc08a371fc1aa1c5f4c0554dd1cdf6b
Jesse Isooba <jesse4isooba@gmail.com>
2023-10-03 10:21:26 +0300

    loop construct updated

---
+++ forloop.js
@@ -0,0 +1,17 @@
+// write a forloop that counts to 3, then start.
+
+for (i = 0; i <= 3; i++) //when the conditional returns false...
+{
+    console.log(i);
+}
+console.log("Start!"); // ...the block of code is exitted
+
+
+
+//write a forloop that counts down to new year
+
+for (j = 10; j > 0; j--) // same senario, but counting down
+{
+    console.log(j);
+}
+console.log("Happy New Year!");
